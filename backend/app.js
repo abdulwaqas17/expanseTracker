16834d7261c05cfd9689c57cc1fc53fe78f06882
Abdul Waqas <www.waqas7172@gmail.com>
2026-06-08 12:13:51 +0500

    add vercel cofiguration

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let app = express();
+require('dotenv').config();
+let connectDB = require('./config/db');
+let authRoutes = require('./routes/authRoutes');
+let userRoutes = require('./routes/userRoutes');
+let expenseRoutes = require('./routes/expenseRoutes');
+let incomeRoutes = require('./routes/incomeRoutes'); 
+
+let cors = require('cors'); 
+
+app.use(express.json()); 
+
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000'
+// };
+
+app.use(cors());
+
+// to connect mongo db
+connectDB();
+
+// Routes
+app.use('/', authRoutes);
+app.use('/', userRoutes);
+app.use('/', expenseRoutes);
+app.use('/', incomeRoutes);
+
+module.exports = app;
